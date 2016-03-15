e32638ddb2060ac8a872efdfe970a10910b0ca18
LiMao <limao@iot.chinamobile.com>
2016-03-15 15:49:49 +0800

    v1.0

---
+++ web_request_test/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace web_request_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.tb_post_data = new System.Windows.Forms.TextBox();
+            this.tb_url = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_method = new System.Windows.Forms.TextBox();
+            this.tb_header_ext = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lb_info = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tb_timeout = new System.Windows.Forms.TextBox();
+            this.tb_wb1 = new System.Windows.Forms.TextBox();
+            this.ck_tb = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(536, 411);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 31);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "开始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.Location = new System.Drawing.Point(12, 12);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(608, 293);
+            this.webBrowser1.TabIndex = 2;
+            // 
+            // tb_post_data
+            // 
+            this.tb_post_data.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.tb_post_data.Location = new System.Drawing.Point(12, 314);
+            this.tb_post_data.Multiline = true;
+            this.tb_post_data.Name = "tb_post_data";
+            this.tb_post_data.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tb_post_data.Size = new System.Drawing.Size(231, 111);
+            this.tb_post_data.TabIndex = 1;
+            this.tb_post_data.Text = "POST DATA";
+            // 
+            // tb_url
+            // 
+            this.tb_url.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.tb_url.Location = new System.Drawing.Point(314, 314);
+            this.tb_url.Name = "tb_url";
+            this.tb_url.Size = new System.Drawing.Size(306, 21);
+            this.tb_url.TabIndex = 3;
+            this.tb_url.Text = "http://";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(279, 323);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "url:";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(261, 400);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "method:";
+            // 
+            // tb_method
+            // 
+            this.tb_method.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.tb_method.Location = new System.Drawing.Point(314, 391);
+            this.tb_method.Name = "tb_method";
+            this.tb_method.Size = new System.Drawing.Size(72, 21);
+            this.tb_method.TabIndex = 5;
+            this.tb_method.Text = "POST";
+            // 
+            // tb_header_ext
+            // 
+            this.tb_header_ext.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.tb_header_ext.Location = new System.Drawing.Point(314, 355);
+            this.tb_header_ext.Name = "tb_header_ext";
+            this.tb_header_ext.Size = new System.Drawing.Size(306, 21);
+            this.tb_header_ext.TabIndex = 3;
+            this.tb_header_ext.Text = "AAA: BBB";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(249, 352);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 24);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "headers\r\next data:";
+            // 
+            // lb_info
+            // 
+            this.lb_info.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lb_info.AutoSize = true;
+            this.lb_info.Location = new System.Drawing.Point(261, 430);
+            this.lb_info.Name = "lb_info";
+            this.lb_info.Size = new System.Drawing.Size(29, 12);
+            this.lb_info.TabIndex = 7;
+            this.lb_info.Text = "就绪";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(405, 388);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 24);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "timeout\r\n(ms):";
+            // 
+            // tb_timeout
+            // 
+            this.tb_timeout.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.tb_timeout.Location = new System.Drawing.Point(458, 391);
+            this.tb_timeout.Name = "tb_timeout";
+            this.tb_timeout.Size = new System.Drawing.Size(72, 21);
+            this.tb_timeout.TabIndex = 8;
+            this.tb_timeout.Text = "60000";
+            // 
+            // tb_wb1
+            // 
+            this.tb_wb1.Location = new System.Drawing.Point(12, 12);
+            this.tb_wb1.Multiline = true;
+            this.tb_wb1.Name = "tb_wb1";
+            this.tb_wb1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tb_wb1.Size = new System.Drawing.Size(608, 293);
+            this.tb_wb1.TabIndex = 10;
+            // 
+            // ck_tb
+            // 
+            this.ck_tb.AutoSize = true;
+            this.ck_tb.Location = new System.Drawing.Point(546, 389);
+            this.ck_tb.Name = "ck_tb";
+            this.ck_tb.Size = new System.Drawing.Size(84, 16);
+            this.ck_tb.TabIndex = 11;
+            this.ck_tb.Text = "显示为文本";
+            this.ck_tb.UseVisualStyleBackColor = true;
+            this.ck_tb.CheckedChanged += new System.EventHandler(this.ck_tb_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 451);
+            this.Controls.Add(this.ck_tb);
+            this.Controls.Add(this.tb_wb1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tb_timeout);
+            this.Controls.Add(this.lb_info);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tb_method);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_header_ext);
+            this.Controls.Add(this.tb_url);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.tb_post_data);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Restful Http调试工具";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.TextBox tb_post_data;
+        private System.Windows.Forms.TextBox tb_url;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb_method;
+        private System.Windows.Forms.TextBox tb_header_ext;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lb_info;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tb_timeout;
+        private System.Windows.Forms.TextBox tb_wb1;
+        private System.Windows.Forms.CheckBox ck_tb;
+    }
+}
+
---
+++ web_request_test/Form1.cs
@@ -0,0 +1,195 @@
+﻿/**
+ * REST-HTTP-Debugger
+ * @author limao (limao777@126.com)
+ * @license BSD 3-Clause, http://opensource.org/licenses/BSD-3-Clause
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Threading;
+
+namespace web_request_test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string WebRequest(string method, string url, string postData)
+        {
+            HttpWebRequest webRequest = null;
+            StreamWriter requestWriter = null;
+            string responseData = "";
+
+            webRequest = System.Net.WebRequest.Create(url) as HttpWebRequest;
+            webRequest.Method = method.ToString();
+            webRequest.ServicePoint.Expect100Continue = false;
+            webRequest.UserAgent  = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.93 Safari/537.36";
+            if (tb_timeout.Text != "" && tb_timeout.Text != "0")
+            {
+                webRequest.Timeout = Convert.ToInt32(tb_timeout.Text);
+            }
+
+            if (tb_header_ext.Text != "")
+            {
+                webRequest.Headers.Add(tb_header_ext.Text);
+            }
+
+            if (method == "POST" || method == "PUT")
+            {
+                if (method == "PUT")
+                {
+                    webRequest.ContentType = "text/xml";
+                    webRequest.Method = "PUT";
+                }
+                else
+                    webRequest.ContentType = "application/x-www-form-urlencoded";
+                //webRequest.ContentType = "multipart/form-data";
+
+                //POST the data.
+                requestWriter = new StreamWriter(webRequest.GetRequestStream());
+                try
+                {
+                    requestWriter.Write(postData);
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+                finally
+                {
+                    requestWriter.Close();
+                    requestWriter = null;
+                }
+            }
+
+            responseData = WebResponseGet(webRequest);
+
+            webRequest = null;
+
+            return responseData;
+
+        }
+
+        public string WebResponseGet(HttpWebRequest webRequest)
+        {
+            StreamReader responseReader = null;
+            string responseData = "";
+
+            try
+            {
+                responseReader = new StreamReader(webRequest.GetResponse().GetResponseStream());
+                responseData = responseReader.ReadToEnd();
+
+
+                webRequest.GetResponse().GetResponseStream().Close();
+                responseReader.Close();
+                responseReader = null;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {               
+                
+            }
+
+            return responseData;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lb_info.Text = "开始……";
+
+            Thread th_request;
+            th_request = new Thread((ThreadStart)delegate()
+            {
+                do_th_request();
+            });
+            th_request.Start();
+
+            lb_info.Text = "处理中……";
+        }
+
+        private void do_th_request()
+        {
+
+
+
+                string res = WebRequest(tb_method.Text, tb_url.Text, tb_post_data.Text);
+                //       textBox1.Text = res;
+
+
+                webBrowser1.ScriptErrorsSuppressed = true; //关闭脚本错误提示
+                webBrowser1.DocumentText = res;
+
+                //webBrowser1.Document.Write(res);
+                webBrowser1.Refresh();
+                this.Invoke((MethodInvoker)delegate()
+                {
+                    tb_wb1.Text = res + "";
+                    lb_info.Text = "完成!";
+                });
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (ck_tb.Checked == true)
+            {
+                tb_wb1.Visible = true;
+            }
+            else {
+                tb_wb1.Visible = false;
+            }
+            if (File.Exists("env.ini")) {
+                StreamReader br = new StreamReader("env.ini");
+                try
+                {
+                    tb_url.Text = br.ReadLine();
+                    tb_header_ext.Text = br.ReadLine();
+                    tb_method.Text = br.ReadLine();
+                    tb_timeout.Text = br.ReadLine();
+                    tb_post_data.Text = br.ReadLine();
+
+                }
+                catch { }
+                finally
+                {
+                    br.Close();
+                    br.Dispose();
+                }
+            }
+        }
+
+        private void ck_tb_CheckedChanged(object sender, EventArgs e)
+        {
+            if (ck_tb.Checked == true)
+            {
+                tb_wb1.Visible = true;
+            }
+            else
+            {
+                tb_wb1.Visible = false;
+            }
+        }
+
+
+
+
+
+
+
+    }
+}
